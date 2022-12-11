91af48fb4cf970716b101ab089e1bd159897f4c7
Magnus Rapp <magnus.rapp@leanon.se>
2022-12-11 20:55:22 +0100

    new build

---
+++ game/precache-manifest.90f33d7fc2bf2404901a9bfc19a675a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913e65186fd459a5294a7e47db523b0f",
+    "url": "/zet/game/index.html"
+  },
+  {
+    "revision": "f374a06d7e82257c05d2",
+    "url": "/zet/game/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "34bb49613a2d9f1ad5ed",
+    "url": "/zet/game/static/js/2.da28c763.chunk.js"
+  },
+  {
+    "revision": "1e073c11e0b6bc84c8eb8bc41f6bb7c4",
+    "url": "/zet/game/static/js/2.da28c763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f374a06d7e82257c05d2",
+    "url": "/zet/game/static/js/main.2301c531.chunk.js"
+  },
+  {
+    "revision": "2ab2ff4da121f772dc1d",
+    "url": "/zet/game/static/js/runtime-main.9e68cdab.js"
+  }
+]);
