db4214a8aeb653b9ecea8fd0eb77ebd2074d60bf
rappzons <44640968+rappzons@users.noreply.github.com>
2022-12-11 20:21:19 +0100

    uploaded buildt game

---
+++ game/precache-manifest.23f1040cc1f45999d1e5ffffbea57c3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a81fb56f06e0a37de14e3da2a621dbd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e2b2161d74b3de3b810",
+    "url": "/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "34bb49613a2d9f1ad5ed",
+    "url": "/static/js/2.da28c763.chunk.js"
+  },
+  {
+    "revision": "1e073c11e0b6bc84c8eb8bc41f6bb7c4",
+    "url": "/static/js/2.da28c763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e2b2161d74b3de3b810",
+    "url": "/static/js/main.f806159c.chunk.js"
+  },
+  {
+    "revision": "b86db83ea67b170a329d",
+    "url": "/static/js/runtime-main.fb40b934.js"
+  }
+]);
